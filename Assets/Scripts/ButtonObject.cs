f65fab1f4943b7d4005dcd1ce5657032d55349a7
juninjune <r0otstat1c@naver.com>
2020-07-27 16:54:34 +0900

    Prototype build

--- Assets/Scripts/ButtonObject.cs
+++ Assets/Scripts/ButtonObject.cs
@@ -2,8 +2,16 @@
 
 public class ButtonObject : MonoBehaviour
 {
+    public Animator animator;
+
     public void LoadGame()
     {
         SceneLoader.Instance.LoadNewScene("FactoryBuild");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        LoadGame();
+        animator.SetTrigger("Click");
     }
 }
